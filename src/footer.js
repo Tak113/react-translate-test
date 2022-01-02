ca0148525e9e67c07cdcecc90fde972d8cfa88b6
EC2 Default User <ec2-user@ip-172-31-29-48.ap-northeast-1.compute.internal>
2022-01-02 09:45:38 +0000

    MOD: footer btn-group. add go to special order.

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,10 @@ import './footer.scss';
 function Footer(props) {
   return (
       <footer className="gfooter">
-        <p className="back"><a href="https://shop.mionosuke.com/">Back to Shop</a></p>
+        <div className="btn-group">
+          <a href="https://shop.mionosuke.com/pages/special-order">Go to Special Order</a>
+          <a href="https://shop.mionosuke.com/" className="back">Back to Shop</a>
+        </div>
         <a href="https://shop.mionosuke.com/pages/contact">contact</a>
         /
         <a href="https://shop.mionosuke.com/pages/terms">terms & conditions</a>
